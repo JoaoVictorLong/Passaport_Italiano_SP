68b11351fd548845eea140db7843763a8c1716e8
JoaoVictorLong <joao.vitor.longo@hotmail.com>
2024-10-16 15:03:32 -0300

    Add options to help in Linux's logrotate

--- log.py
+++ log.py
@@ -1,6 +1,13 @@
-import logging
+import logging.handlers
 
-logging.basicConfig(level=logging.INFO, filename='appontment.log', format="%(asctime)s - %(levelname)s - %(message)s")
+#logging.basicConfig(level=logging.INFO, filename='appontment.log', format="%(asctime)s - %(levelname)s - %(message)s")
+#Definindo um wait para o log achar novos arquivos
+
+log_wait = logging.handlers.WatchedFileHandler('appontment.log')
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger()
+logger.addHandler(log_wait)
+logger.setLevel(logging.INFO)
 
 def page_stop(messagem):
     logging.warning(messagem)
